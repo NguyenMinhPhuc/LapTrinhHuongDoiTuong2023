8e8c415991ec53576b7fa09eebdaa9280c1ddeef
Nguyen Minh Phuc <nmphuc@outlook.com>
2023-08-26 10:49:23 +0700

    BuoiHoc26-8

---
+++ Exercises/cs01_LopVaDoiTuong/QuanLySinhVien/Cls_Main.cs
@@ -0,0 +1,17 @@
+using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuanLySinhVien
+{
+    public static class Cls_Main
+    {
+       public string static path="";
+
+       public static void DocThongTinHeThong(){
+
+       }
+    }
+}
